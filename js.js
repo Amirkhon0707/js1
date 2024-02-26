3786c1ae17a0f944491dd17d9c079019b82b9d4c
Amirkhon0707 <158686103+Amirkhon0707@users.noreply.github.com>
2024-02-26 12:54:53 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,29 @@
+var str = prompt('Введите своё имя');
+
+console.log('Меня зовут ' + str);
+
+var age = +prompt('Введите свой возраст')
+
+console.log('Мне ' + age + ' лет');
+
+var example = +prompt('Решите пример 40 + 20')
+
+console.log('Пример 1: 40 + 20 = ' + (40 + 20) + ' Ваш ответ ' + example)
+
+var examples = +prompt('Решите пример 30 - 15')
+
+console.log('Пример 2: 30 - 15 = ' + (30 - 15) + ' Ваш ответ ' + examples)
+
+var examplet = +prompt('Решите пример 50 * 2')
+
+console.log('Пример 3: 50 * 2 = ' + (50 * 2) + ' Ваш ответ ' + examplet)
+
+var examplef = +prompt('Решите пример 40 / 20')
+
+console.log('Пример 4: 40 / 20 = ' + (40 / 20) + ' Ваш ответ ' + examplef)
+
+var examplel = +prompt('Решите пример 10 % 3')
+
+console.log('Пример 5: 10 % 3 = ' + (10 % 3) + ' Ваш ответ ' + examplel)
+
+alert('Откройте консоль для просмотра правильных ответов');
